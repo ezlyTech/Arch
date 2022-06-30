f0a95df47c49b6ed734f79ad65c6cf29c8b14562
Eliz Marie Manalo <tech.ezly@gmail.com>
2022-07-01 02:07:54 +0800

    Fix herobanner splide pagination

--- app/js/script.js
+++ app/js/script.js
@@ -11,6 +11,21 @@ document.querySelector('.header__button')
 
 // Herobanner
 document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide( '.splide' );
+    var splide = new Splide( '.splide', {
+        type    : 'loop',
+        autoplay: true,
+    });
+
+    splide.on( 'pagination:mounted', function ( data ) {
+        // You can add your class to the UL element
+        data.list.classList.add( 'splide__pagination--custom' );
+      
+        // `items` contains all dot items
+        data.items.forEach( function ( item ) {
+          item.button.textContent = String( item.page + 1 );
+        } );
+    } );
+
+
     splide.mount();
   } );
