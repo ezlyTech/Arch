1cd33370ad95e3941412217043f045a11da9025c
Eliz Marie Manalo <tech.ezly@gmail.com>
2022-07-01 22:07:30 +0800

    add portfolio page

--- app/js/script.js
+++ app/js/script.js
@@ -10,7 +10,29 @@ document.querySelector('.header__button')
 
 
 // Herobanner
-document.addEventListener( 'DOMContentLoaded', function() {
+// var herobanner = document.querySelectorAll('.herobanner');
+// herobanner.addEventListener( 'load', function() {
+//     var splide = new Splide( '.splide', {
+//         type    : 'loop',
+//         autoplay: true,
+//     });
+
+//     splide.on( 'pagination:mounted', function ( data ) {
+//         // You can add your class to the UL element
+//         data.list.classList.add( 'splide__pagination--custom' );
+      
+//         // `items` contains all dot items
+//         data.items.forEach( function ( item ) {
+//           item.button.textContent = String( item.page + 1 );
+//         } );
+//     } );
+
+
+//     splide.mount();
+//   } );
+
+
+$('.herobanner').ready(function() {
     var splide = new Splide( '.splide', {
         type    : 'loop',
         autoplay: true,
@@ -28,4 +50,4 @@ document.addEventListener( 'DOMContentLoaded', function() {
 
 
     splide.mount();
-  } );
+}) 
